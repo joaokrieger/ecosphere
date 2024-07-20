4482f163bf7dbc6eef74395e165bb70d96760a62
João Eduardo Krieger <joaoeduardokrieger123@gmail.com>
2024-07-20 20:28:49 -0300

    Mecânica de presas e adição de arvores

--- Assets/_ECOSPHERE_/Scripts/Animais/Presa.cs
+++ Assets/_ECOSPHERE_/Scripts/Animais/Presa.cs
@@ -2,26 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Tilemaps;
 
 public class Presa : MonoBehaviour
 {
-    public float velocidade = 5f;
     public float distanciaConsumo = 1f;
-    public Transform gramaAlvo;
+    public float tempoConsumo = 2f;
+
+    private Transform gramaAlvo;
     private NavMeshAgent agent;
+    private Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+    private Animator _animator;
+    private bool consumindoGrama;
+
+    public Tilemap tilemapCampo;
+    public Vector2 areaMin;
+    public Vector2 areaMax;
+
+    private bool estaRondando = false;
+    public float tempoRonda;
+    public float tempoParado;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
-        agent.updateUpAxis = false;      
+        agent.updateUpAxis = false;
+        consumindoGrama = false;
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gramaAlvo != null)
+        if (gramaAlvo != null && !consumindoGrama)
         {
             agent.SetDestination(gramaAlvo.position);
 
@@ -29,26 +48,92 @@ public class Presa : MonoBehaviour
             float distanciaParaGrama = Vector2.Distance(transform.position, gramaAlvo.position);
             if (distanciaParaGrama <= distanciaConsumo)
             {
-                ConsumirGrama();
+                StartCoroutine(ConsumirGrama());
             }
+        }
+        else
+        {
+            if (!estaRondando)
+            {
+                estaRondando = true;
+                StartCoroutine(RealizaRonda());
+            }
+        }
+
+        CorrigeLocomocao();
+    }
+
+    private void CorrigeLocomocao()
+    {
+        Vector3 velocidade = agent.velocity;
+
+        if (velocidade.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (velocidade.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (velocidade.magnitude > 0.1f)
+        {
+            _animator.SetBool("estaAndando", true);
+        }
+        else
+        {
+            _animator.SetBool("estaAndando", false);
+        }
+    }
+
+    IEnumerator RealizaRonda()
+    {
+        while (true)
+        {
+            // Espera pelo tempo definido em tempoRonda
+            yield return new WaitForSeconds(tempoRonda);
+
+            float randomX = Random.Range(areaMin.x, areaMax.x);
+            float randomY = Random.Range(areaMin.y, areaMax.y);
+            Vector3 posicaoAleatoria = new Vector3(randomX, randomY, 0);
+
+            Vector3Int posicaoCelula = tilemapCampo.WorldToCell(posicaoAleatoria);
+
+            // Garante que a posição aleatória seja válida
+            while (!tilemapCampo.HasTile(posicaoCelula))
+            {
+                randomX = Random.Range(areaMin.x, areaMax.x);
+                randomY = Random.Range(areaMin.y, areaMax.y);
+                posicaoAleatoria = new Vector3(randomX, randomY, 0);
+                posicaoCelula = tilemapCampo.WorldToCell(posicaoAleatoria);
+            }
+            agent.SetDestination(posicaoAleatoria);
+
+            // Espera pelo tempo definido em tempoParado
+            yield return new WaitForSeconds(tempoParado);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D colider)
     {
-        if (colider.CompareTag("Grama"))
+        if (colider.CompareTag("Grama") && gramaAlvo == null)
         {
             gramaAlvo = colider.transform;
         }
     }
 
-    void ConsumirGrama()
+    private IEnumerator ConsumirGrama()
     {
         if (gramaAlvo != null)
         {
-            Debug.Log("Destruindo grama: " + gramaAlvo.name);
+            consumindoGrama = true;
+            _animator.SetBool("estaAndando", false);
+            _animator.SetBool("estaPastando", true);
+            yield return new WaitForSeconds(tempoConsumo);
             Destroy(gramaAlvo.gameObject);
             gramaAlvo = null;
+            _animator.SetBool("estaPastando", false);
+            consumindoGrama = false;
         }
     }
 }
